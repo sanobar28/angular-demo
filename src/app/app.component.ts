7fc2ae7f6f7eddc990cd23bdc0bad915288cf924
sanobar28 <sanobar.mujawar@gmail.com>
2021-08-21 18:25:06 +0530

    added: input event binding using regex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ export class AppComponent {
   title = 'hello-world';
   imgUrl = "../assets/BL_logo_square_jpg.jpg"
   url = "https://www.bridgelabz.com"
-  userName: String = "Sanobar";
+  userName: string = "";
+  nameError: string = "";
 
   ngOnInit(): void {
     this.title = "Hello from Bridgelabz";
@@ -18,5 +19,15 @@ export class AppComponent {
   onClick($event) {
     console.log("image is clicked",  $event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event) {
+    console.log("change event occured", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-z\\s]{2,}$');
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect";
   }
 }
